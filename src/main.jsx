1c9d59f0f10b2481aeedd9b49a8f7fdcd1b245e0
A-Richdom <anigilajeridwanhorpeyemy@gmail.com>
2024-08-05 12:58:08 +0100

    Pass loading prop to LoadingSpinner component

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store, persistor } from './Components/FEATURES/Store.js'
 import { PersistGate } from 'redux-persist/integration/react'
+import LoadingSpinner from './Components/LoadingSpinner.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<LoadingSpinner />} persistor={persistor}>
 
         <App />
 
