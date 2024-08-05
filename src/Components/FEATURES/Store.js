d6e8a20e15042d7b4c3df29f6cb8c353fb6ae698
A-Richdom <anigilajeridwanhorpeyemy@gmail.com>
2024-08-05 12:18:32 +0100

    Configure Redux Persist to use local storage for state persistence

--- src/Components/FEATURES/Store.js
+++ src/Components/FEATURES/Store.js
@@ -1,13 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
+import { storage } from 'redux-persist/lib/storage'; //default to local storage for web
 import dishesReducer from './DishesSlice';
 
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, dishesReducer);
+
 export const store = configureStore({
     reducer: {
-        dishes: dishesReducer,
+        dishes: persistedReducer,
     },
 });
+
+
+export const persistor = persistStore(store);
+
 
 //Subscribe to the store to log state changes
 store.subscribe(() => {
