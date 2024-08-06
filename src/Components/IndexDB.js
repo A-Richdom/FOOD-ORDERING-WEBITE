5e4a341d3c96bcf7492040574a9a5e18c5a5125f
A-Richdom <anigilajeridwanhorpeyemy@gmail.com>
2024-08-06 15:12:29 +0100

    install idb library for IndexedDB management

---
+++ src/Components/IndexDB.js
@@ -0,0 +1,36 @@
+import { openDB } from 'idb';
+
+const DB_NAME = 'foodOrdering';
+const STORE_NAME = 'dishes';
+
+export const initDB = async () => {
+  return openDB(DB_NAME, 1, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
+      }
+    },
+  });
+};
+
+export const saveDish = async (dish) => {
+  const db = await initDB();
+  const tx = db.transaction(STORE_NAME, 'readwrite');
+  await tx.objectStore(STORE_NAME).put(dish);
+  await tx.done;
+};
+
+export const getDishes = async () => {
+  const db = await initDB();
+  const tx = db.transaction(STORE_NAME, 'readonly');
+  const dishes = await tx.objectStore(STORE_NAME).getAll();
+  await tx.done;
+  return dishes;
+};
+
+export const deleteDish = async (id) => {
+  const db = await initDB();
+  const tx = db.transaction(STORE_NAME, 'readwrite');
+  await tx.objectStore(STORE_NAME).delete(id);
+  await tx.done;
+};
